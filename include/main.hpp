55922ecaa93805e8f59d72464177343d242e78c2
Kaitlyn <87990853+ItsKaitlyn03@users.noreply.github.com>
2022-04-03 10:52:00 -0400

    Use codegen fields, for performance reasons

--- include/main.hpp
+++ include/main.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#define USE_CODEGEN_FIELDS
+
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "beatsaber-hook/shared/utils/logging.hpp"
