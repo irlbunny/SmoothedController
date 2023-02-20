415aa2f9c85dc363ebc776414dff4364b77bdf75
NuggoDEV <NuggoDEV@gmail.com>
2023-02-20 16:58:25 +1030

    Update to 1.27.0

--- include/SmoothedControllerConfig.hpp
+++ include/SmoothedControllerConfig.hpp
@@ -7,11 +7,4 @@ DECLARE_CONFIG(SmoothedControllerConfig,
     CONFIG_VALUE(PositionSmoothing, float, "PositionSmoothing", 3.f, "How much to smooth position changes by");
     CONFIG_VALUE(RotationSmoothing, float, "RotationSmoothing", 12.f, "How much to smooth rotation changes by");
     CONFIG_VALUE(SmallMovementThresholdAngle, float, "SmallMovementThresholdAngle", 6.f, "Angle difference to consider a 'small' movement. The less shaky your hands are the lower you probably want to put this. This is probably the primary value you want to tweak");
-
-    CONFIG_INIT_FUNCTION(
-        CONFIG_INIT_VALUE(Enabled);
-        CONFIG_INIT_VALUE(PositionSmoothing);
-        CONFIG_INIT_VALUE(RotationSmoothing);
-        CONFIG_INIT_VALUE(SmallMovementThresholdAngle);
-    )
 )
